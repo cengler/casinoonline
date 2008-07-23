9ed385262dd30be0c0ea878364a576a7f95e3974
mdelosaalvarez <mdelosaalvarez@396af325-f14a-0410-ba05-f70d8a51d37d>
2008-07-23 23:32:17 +0000

    Mensajes de craps: solo falta un xml para estado casino y parsearlo.

--- trunk/Tp3/codigo/src/craps/msg/MSGEstadoCraps.java
+++ trunk/Tp3/codigo/src/craps/msg/MSGEstadoCraps.java
@@ -1,7 +1,44 @@
 package craps.msg;
 
+
+import java.util.List;
+
+import casino.msg.estadoCasino.MSGJugador;
+//import casino.msg.estadoCasino.MSGUltimoTiroCraps;
+
 public class MSGEstadoCraps extends MSGCraps {
 
+	
+	private List<MSGJugador> jugadores;
+	private MSGProxTiro proximoTiro;
+	private MSGUltimoTiro ultimoTiro;
+	private MSGApuestasVigentes apuestasVigentes;
+	
+	
+	public MSGApuestasVigentes getApuestasVigentes() {
+		return apuestasVigentes;
+	}
+	public void setApuestasVigentes(MSGApuestasVigentes apuestasVigentes) {
+		this.apuestasVigentes = apuestasVigentes;
+	}
+	public List<MSGJugador> getJugadores() {
+		return jugadores;
+	}
+	public void setJugadores(List<MSGJugador> jugadores) {
+		this.jugadores = jugadores;
+	}
+	public MSGProxTiro getProximoTiro() {
+		return proximoTiro;
+	}
+	public void setProximoTiro(MSGProxTiro proximoTiro) {
+		this.proximoTiro = proximoTiro;
+	}
+	public MSGUltimoTiro getUltimoTiro() {
+		return ultimoTiro;
+	}
+	public void setUltimoTiro(MSGUltimoTiro ultimoTiro) {
+		this.ultimoTiro = ultimoTiro;
+	}
 	
 
 }

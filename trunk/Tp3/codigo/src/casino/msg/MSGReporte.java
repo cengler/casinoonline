694a3bdacd60fa4591c0494718fa97c8d2197069
isisaquino <isisaquino@396af325-f14a-0410-ba05-f70d8a51d37d>
2008-07-25 00:37:10 +0000

    agregue reportes

---
+++ trunk/Tp3/codigo/src/casino/msg/MSGReporte.java
@@ -0,0 +1,13 @@
+package casino.msg;
+
+public abstract class MSGReporte {
+		private String aceptado;
+
+		public String getAceptado() {
+			return aceptado;
+		}
+
+		public void setAceptado(String aceptado) {
+			this.aceptado = aceptado;
+		}
+}

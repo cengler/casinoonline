1b13d67757c609276d520c87ddf6392c298a7e4e
caeycae <caeycae@396af325-f14a-0410-ba05-f70d8a51d37d>
2008-07-25 21:00:17 +0000

    * comentarios y yerbas

--- Tp3/codigo/src/casino/IInvitado.java
+++ Tp3/codigo/src/casino/IInvitado.java
@@ -1,9 +1,27 @@
 package casino;
 
+/**
+ * Interface de un jugador invitado. 
+ * Para limitar a los clientes de la manipulacion erronea del invitado
+ * 
+ * @author Grupo2
+ *
+ */
 public interface IInvitado extends ICliente {
 
+	/**
+	 * Geter del id de la terminal virtual desde la cual
+	 * se conecta el cliente invitado.
+	 * 
+	 * @return el id de la terminal virtual
+	 */
 	public int getIdVirt();
 	
+	/**
+	 * Geter del nombre del invitado.
+	 * 
+	 * @return el nombre del invitado
+	 */
 	public String getNombre();
 
 }

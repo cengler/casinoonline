d61e67afbe71f02e99ab9ef6bef5b6595b71ab51
caeycae <caeycae@396af325-f14a-0410-ba05-f70d8a51d37d>
2008-07-29 00:43:25 +0000

    * termino de testear tirarCraps con el seleccionador de tirador ( solo falta pagar apuestas)

--- trunk/Tp3/codigo/src/craps/SelectorResCrapsModoNormal.java
+++ trunk/Tp3/codigo/src/craps/SelectorResCrapsModoNormal.java
@@ -1,13 +1,15 @@
 package craps;
 
+import java.util.Random;
+
+import org.apache.log4j.Logger;
+
 public class SelectorResCrapsModoNormal implements ISelectorResCraps {
 
+	private static Logger logger = Logger.getLogger(SelectorResCrapsModoNormal.class);
 	private static SelectorResCrapsModoNormal instance;
 	
-	private SelectorResCrapsModoNormal()
-	{
-		
-	}
+	private SelectorResCrapsModoNormal(){}
 	
 	public static SelectorResCrapsModoNormal getInstance()
 	{
@@ -16,8 +18,17 @@ public class SelectorResCrapsModoNormal implements ISelectorResCraps {
 		return instance;
 	}
 
-	public ResultadoCraps getResult() {
-		// TODO Auto-generated method stub
-		return null;
+	public ResultadoCraps getResult() 
+	{	
+		ResultadoCraps res = null;
+		Random generator = new Random();
+		int dado1 = generator.nextInt(6) + 1;
+		int dado2 = generator.nextInt(6) + 1;
+		try {
+			 res = new ResultadoCraps(dado1, dado2);
+		} catch (CrapsException e) {
+			logger.fatal("MAL CODIFICADO EL RANDOM");
+		}
+		return res;
 	}
 }

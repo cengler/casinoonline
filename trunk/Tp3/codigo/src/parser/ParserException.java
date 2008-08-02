2fd418f112ddafdc4d9cc66ae72164897c6a38c3
caeycae <caeycae@396af325-f14a-0410-ba05-f70d8a51d37d>
2008-08-02 23:27:14 +0000

    * depuracion funcional. paso3

--- trunk/Tp3/codigo/src/parser/ParserException.java
+++ trunk/Tp3/codigo/src/parser/ParserException.java
@@ -4,22 +4,18 @@ package parser;
 public class ParserException extends Exception {
 
 	public ParserException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public ParserException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ParserException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ParserException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
 
 }
--- trunk/Tp3/codigo/test/mensajes/TestEstadoCasino.java
+++ trunk/Tp3/codigo/test/mensajes/TestEstadoCasino.java
@@ -1,20 +1,47 @@
 package mensajes;
 
+import core.FileMessage;
+import core.IMessage;
+import core.MensajeroException;
+import craps.ManejadorMesaCraps;
+import craps.MesaCraps;
+import parser.ParserCasino;
+import parser.ParserException;
+import casino.Jugador;
+import casino.ManejadorCasino;
+import casino.ManejadorJugador;
+import casino.msg.estadoCasino.MSGEstadoCasino;
+
 
 public class TestEstadoCasino {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MensajeroException, ParserException {
 		
-		/*InputStream is8;
-		try 
-		{
-			is8 = new FileInputStream("D:/casino/codigo/xml/casino/estadoCasino010041.xml");
-			MSGEstadoCasino msg = (MSGEstadoCasino)ParserCasino.getInstance().parse(is8);
-			ParserCasino.getInstance().renderizar(msg, "D:/casino/codigo/xml/casino/estadoCasino010041P.xml");
-		} 
-		catch (FileNotFoundException e) 
-		{
-			e.printStackTrace();
-		}*/
+		ManejadorJugador.getInstance().getManejadores().add(ManejadorMesaCraps.getInstance());
+		
+		MSGEstadoCasino mensaje = new MSGEstadoCasino();
+		mensaje.setUsuario("pepe");
+		mensaje.setVTerm("50");
+		
+		Jugador jug = new Jugador();
+		jug.setNombre("pepe");
+		jug.setIdVirt("50");
+		jug.setLogeado(true);
+		ManejadorJugador.getInstance().getJugadores().add(jug);
+		
+		MesaCraps mesa = new MesaCraps(1);
+		mesa.setAbierta(true);
+		mesa.getJugadores().add(jug);
+		mesa.setTirador(jug);
+		ManejadorMesaCraps.getInstance().getMesas().add(mesa);
+		
+		ManejadorCasino.getInstance().estadoCasino(mensaje);
+		
+		
+		IMessage msg = new FileMessage("D:/casino/test/estado.xml");
+		
+		ParserCasino.getInstance().renderizar(mensaje, msg);
+		
+		System.out.println("\n" + msg.getData());
 	}
 }

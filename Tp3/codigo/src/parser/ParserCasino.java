bfe6011fc9510e79b392f09f777a3bb2dd52aba9
caeycae <caeycae@396af325-f14a-0410-ba05-f70d8a51d37d>
2008-08-03 00:08:01 +0000

    * arreglo bug de idvirt en jugador

--- Tp3/codigo/src/parser/ParserCasino.java
+++ Tp3/codigo/src/parser/ParserCasino.java
@@ -1,5 +1,7 @@
 package parser;
 
+import org.apache.log4j.Logger;
+
 import casino.msg.MSGCasino;
 import casino.msg.MSGEntradaCasino;
 import casino.msg.MSGSalidaCasino;
@@ -12,13 +14,16 @@ import casino.msg.estadoCasino.MSGObservador;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
+import core.IMessage;
+
 import craps.msg.MSGProxTiro;
 import craps.msg.MSGUltimoTiro;
 
 public class ParserCasino extends Parser {
 	
-	//private static Logger logger = Logger.getLogger(ParserCasino.class);
+	private static Logger logger = Logger.getLogger(ParserCasino.class);
 	private static ParserCasino instance;
+	private static XStream xstream;
 	
 	private ParserCasino()
 	{
@@ -49,6 +54,37 @@ public class ParserCasino extends Parser {
 		xstream.alias("pozos", MSGPozo.class);
 		xstream.aliasAttribute(MSGObservador.class, "nombre", "nombre");
 		*/
+	}
+	
+	public Object parse(IMessage is) throws ParserException
+	{	
+		try
+		{
+			logger.debug("parseando... " + is.getName() + "\n ------------------- \n" + 
+					is.getData() + " ------------------- ");
+			Object message; 
+			String s = is.getData();
+			message = xstream.fromXML(s.trim());
+			logger.debug("parseando: " + message + "\n P>------------------ \n" + 
+					is.getData() + " P>------------------ ");
+			return message;
+		}catch(RuntimeException re)
+		{
+			logger.error("SE RE COLGO! " + re.getMessage() + " "+ re.getCause(), re);
+			throw new ParserException(re) ;
+		}
+	}
+	
+	/**
+	 * Senderiza dentro del IMensaje al mensaje recibido como primer parametro.
+	 * 
+	 * @param msg mensaje a renderizar
+	 * @param imsg IMensaje a llenar con la informacion 
+	 * @throws ParserException en caso de un error en el parser
+	 */
+	public void renderizar(Object msg, IMessage imsg) throws ParserException
+	{
+		imsg.setData(xstream.toXML(msg));
 	}
 	
 	public static ParserCasino getInstance()

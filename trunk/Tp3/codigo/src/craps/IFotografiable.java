fdf20c84ae21a5773e37527d43dbcc0183daa90a
caeycae <caeycae@396af325-f14a-0410-ba05-f70d8a51d37d>
2008-08-03 05:54:11 +0000

    * reacomodacion de clases para simplificar dependencias 2  * quito clases huerfanas

---
+++ trunk/Tp3/codigo/src/craps/IFotografiable.java
@@ -0,0 +1,10 @@
+package craps;
+
+import craps.msg.MSGEstadoCraps;
+
+public interface IFotografiable {
+
+	//este metodo lo va a llamar un jugador desde una mesa.
+	MSGEstadoCraps sacarFoto();
+
+}
